1697328a5d31febf23b9244179cba7e04b1f8d4a
trishnendu <trishnendu@outlook.com>
2017-12-16 20:34:14 +0530

    skip already downloaded files added

--- crawler_downloader.py
+++ crawler_downloader.py
@@ -1,13 +1,12 @@
 import urllib.request
 import lxml.html
-import sys, os
+import sys, os, time
 import shutil
 
 #outfile = open("links.txt",'wb')
 media = ["mkv","MKV","MP4","mp4"]
 quality = "720p"
-searchterm = "Dexter"
-chunk_size = 1024*1024  # 1 MBs downloadspeed 
+chunk_size = 256*1024   
 
 class http_response_message:
     def __init__(self, response):
@@ -19,10 +18,13 @@ class http_response_message:
             if len(fields) == 2:
                 self.dict[fields[0]] = fields[1]
         
-def notify(bytes_so_far, total_size):
+def notify(bytes_so_far, total_size, time = None):
     percent = float(bytes_so_far) / total_size
     percent = round(percent*100, 2)
-    sys.stdout.write("Downloaded %0.2f Mbytes of %0.2f Mbytes (%0.2f%%)\r" % (float(bytes_so_far/(1024*1024)), float(total_size/(1024*1024)), percent))
+    if time:
+        sys.stdout.write("Downloaded %0.2f Mbytes of %0.2f Mbytes (%0.2f%%)\tSpeed = %0.2f MBps\r" % (float(bytes_so_far/(1024*1024)), float(total_size/(1024*1024)), percent, float(1/time)))
+    else:
+        sys.stdout.write("Downloaded %0.2f Mbytes of %0.2f Mbytes (%0.2f%%) \r" % (float(bytes_so_far/(1024*1024)), float(total_size/(1024*1024)), percent))
     if bytes_so_far >= total_size:
         sys.stdout.write('\n')    
     
@@ -37,21 +39,27 @@ def download(url, display_hook = None):
     #sys.stdout.write("Found media -> "+outfilename+"  (SIZE "+str(total_size/(1024*1024))+"M)\n")
     if quality not in url:
         return
+    outfilename = os.path.join(os.getcwd(), outfilename)
+    if os.path.exists(outfilename) and os.path.getsize(outfilename) == total_size:
+        sys.stdout.write(outfilename+" already downloaded, skipping... \n")
+        return
     outfp = open(os.path.join(os.getcwd(), outfilename), "wb")
-    #sys.stdout.write("Downloading file to "+os.path.join(os.getcwd(), outfilename)+"\t...\n")
+    sys.stdout.write("Downloading file to "+os.path.join(os.getcwd(), outfilename)+"\t...\n")
+    starttime = time.time()
     while(True):
+        ts = time.time()
         chunk = response.read(chunk_size)
         bytes_so_far += len(chunk)
         if not(chunk):
             break
-        notify(bytes_so_far, total_size)
+        notify(bytes_so_far, total_size, time.time() - ts)
         #sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%) -> " % (bytes_so_far, total_size, float(bytes_so_far*100/total_size)))
         #sys.stdout.write(os.path.join(os.getcwd(), outfilename)+"\n" )
         outfp.write(chunk)
     if total_size != bytes_so_far:
         print("Error in downloading! Aborting :(")
         exit(1)
-    sys.stdout.write("Download complete :) -> "+os.path.join(os.getcwd(), outfilename)+"\n" )
+    sys.stdout.write("Download "+str(float(total_size/(1024*1024)))+" MB in "+str(float(time.time()-starttime)/60)+" Min complete :) -> "+os.path.join(os.getcwd(), outfilename)+"\n" )
     outfp.close()
     return
     
@@ -85,5 +93,6 @@ def crawler(url, download_content = False, crawl_depth = float('inf')):
     for link in links:
         crawler(url+link, download_content = download_content, crawl_depth = crawl_depth - 1)
     if(download_content): os.chdir("..")
-    
-crawler("http://fastdownloadhub.com/Serial/Better Call Saul/", crawl_depth = 1, download_content = True)
+
+os.chdir(os.path.join(os.getcwd(),"Dexter"))    
+crawler("http://fastdownloadhub.com/Serial/Dexter/S02/", download_content = True)
